49c894c32337e9c62549e9817f2da228c44597d2
Alejandro Cordova <galvez_84@outlook.com>
2024-07-13 14:35:58 -0600

    Cambios en las estructura del webpack Mix, cambios visuales en la parte de frontend y backend, Progreso en la estructura de el administrador de contenidos, e integracion de el servidor sftp  - Alejandro ,Julio 13

--- app/Models/ImagenesContenido.php
+++ app/Models/ImagenesContenido.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Programas extends Model
+class ImagenesContenido extends Model
 {
     use HasFactory;
     use SoftDeletes;
